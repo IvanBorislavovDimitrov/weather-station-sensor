15d2492f0e1320ff15eb22255cbbf3c3efeddafe
IvanBorislavovDimitrov <starstrucks199@gmail.com>
2022-04-04 19:07:03 +0100

    Dynamic server hostname resolving

--- bme.py
+++ bme.py
@@ -38,13 +38,15 @@ def get_weather():
 
 @app.route('/start', methods=['POST'])
 def start_sending():
+    
+    server_name = json.loads(request.data)["hostname"]
     global sending
     sending=True
     bashCommand = "hostname -I"
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     hostname = re.findall("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", output.decode('utf-8'))
-    x = threading.Thread(target=thread_function, args=(hostname[0],))
+    x = threading.Thread(target=thread_function, args=(hostname[0], server_name))
     x.start()
     return Response("", mimetype='application/json')
 
@@ -54,15 +56,15 @@ def stop_sending():
     sending=False
     return Response("", mimetype='application/json')
 
-def thread_function(name):
+def thread_function(local_hostname, server_hostname):
     while (sending):
-        print("Thread starting")
+        print("Thread starting! Localhost: " + local_hostname + ". Server host: " + server_hostname)
         data = bme280.sample(bus, address, calibration_params)
         #print(data)
-        w = {'temperature': data.temperature,'humidity': data.humidity, 'pressure': data.pressure, 'raspberryRoute': name}
+        w = {'temperature': data.temperature,'humidity': data.humidity, 'pressure': data.pressure, 'raspberryRoute': local_hostname}
         headers = {'Content-Type': 'application/json'}
         try:
-            r = requests.post('http://192.168.1.3:8080/measurement',json=w, headers=headers, timeout=10) # add authentication
+            r = requests.post('http://'+ server_hostname + ':8080/measurement',json=w, headers=headers, timeout=10) # add authentication
             print(r.text)
         except:
             print("Error occurred while communicating with server")
